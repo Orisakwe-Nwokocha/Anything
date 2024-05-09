59d7cf7527adb0f04de8e17883a09e76efca25f3
Orisakwe Nwokocha <orisakwenwokocha1@gmail.com>
2024-05-09 20:58:54 +0100

    removed redundant declarations for better clarity

--- MrChinedu/test/ModeObject.test.js
+++
@@ -1,7 +0,0 @@
-const {modeObjectOf} = require('../src/ModeObject');
-
-
-test("get mode object", () => {
-    let input = [1, 2, 2, 1, 3];
-    expect(modeObjectOf(input)).toEqual({1: 2, 2: 2, 3: 1});
-})
